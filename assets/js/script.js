a1b45575aa5151d95271fc2e173d622428949b50
tambrulee <thamiltontestrote@gmail.com>
2025-05-21 07:18:32 +0100

    Added date function

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// This function calls the current time and date
+let timeDate = new Date();
+
+document.getElementById("time-date").textContent = timeDate;
+
+// Tests
+// Correct Date?
+console.log(new Date())
