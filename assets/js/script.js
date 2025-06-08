6cc07c4c9acb2d21648dfa4afbe359a2de1bb252
tambrulee <thamiltontestrote@gmail.com>
2025-06-08 19:00:30 +0100

    Inputting template of functions for drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,31 @@ let timeDate = new Date();
 document.getElementById("time-date").textContent = timeDate;
 
 // Tests
+
 // Correct Date?
 console.log(new Date())
+
+// Drag and Drop functionality
+// The following functions allows drag and drop functionality for the user when they click and hold.
+// When the user adds a new task, these functions will add a new task into a default drop zone.
+// Then a new drop zone will be generated beneath
+
+// Allows the user to drag and drop tasks when they click and hold task
+function(dragDrop)
+
+// Adds a new task box to the drop zone on the list
+function(addTask)
+
+// Copies the task box html and adds it to the drop zone on the list
+function(copyTask)
+
+// Replaces the previous dropzone once the task box is created
+function(replaceDropzone)
+
+// Creates a dropzone each time a task box is created
+function(loopDropzone)
+
+
+// Changes the theme
+function(changeTheme)
+
