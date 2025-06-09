9d80ad306aaa58cc599ca91d74859ce2452e8e8b
tambrulee <thamiltontestrote@gmail.com>
2025-06-09 18:07:23 +0100

    Added comments to drag and drop in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,17 @@ console.log(new Date())
 // Drag and Drop functionality
 // The following functions allows drag and drop functionality for the user when they click and hold.
 // When the user adds a new task, these functions will add a new task into a default drop zone.
-// Then a new drop zone will be generated beneath
 
+// Calls the drag and drop API to start
+// For every div with a classification of drag-n-drop the function will apply a data transfer
 document.querySelectorAll(".drag-n-drop").forEach((el) => {
     el.addEventListener("dragstart", (ev) => {
       ev.dataTransfer.setData("text/plain", ev.target.id);
       ev.dataTransfer.effectAllowed = "move";
     });
 
+// Allows the div to be drasgged over the other same classified divs and to be added to the target location
+// Whilst also shifting the existing divs to either side of the div which is being moved
     el.addEventListener("dragover", (ev) => {
       ev.preventDefault();
       ev.target.classList.add("drag-over");
