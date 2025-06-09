20b641a9bc5e9887f4a7b89c409240354e2510b2
tambrulee <thamiltontestrote@gmail.com>
2025-06-09 17:50:25 +0100

    Drag and drop functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,56 @@ console.log(new Date())
 // When the user adds a new task, these functions will add a new task into a default drop zone.
 // Then a new drop zone will be generated beneath
 
-// Allows the user to drag and drop tasks when they click and hold task
-function(dragDrop)
+document.querySelectorAll(".drag-n-drop").forEach((el) => {
+    el.addEventListener("dragstart", (ev) => {
+      ev.dataTransfer.setData("text/plain", ev.target.id);
+      ev.dataTransfer.effectAllowed = "move";
+    });
 
-// Adds a new task box to the drop zone on the list
-function(addTask)
+    el.addEventListener("dragover", (ev) => {
+      ev.preventDefault();
+      ev.target.classList.add("drag-over");
+    });
+
+    el.addEventListener("dragleave", (ev) => {
+      ev.target.classList.remove("drag-over");
+    });
+
+    el.addEventListener("drop", (ev) => {
+      ev.preventDefault();
+      ev.target.classList.remove("drag-over");
+
+      const draggedId = ev.dataTransfer.getData("text/plain");
+      const draggedEl = document.getElementById(draggedId);
+      const dropTarget = ev.target;
+
+      if (dropTarget !== draggedEl) {
+        // Swap elements visually
+        const temp = document.createElement("div");
+        dropTarget.parentNode.insertBefore(temp, dropTarget);
+        draggedEl.parentNode.insertBefore(dropTarget, draggedEl);
+        temp.parentNode.insertBefore(draggedEl, temp);
+        temp.remove();
+      }
+    });
+  });
+  
+  
+  
+  
+    
+
+// Adds a new tzask box to the drop zone on the list
+// function(addTask){}
 
 // Copies the task box html and adds it to the drop zone on the list
-function(copyTask)
+// function(copyTask){}
 
 // Replaces the previous dropzone once the task box is created
-function(replaceDropzone)
+// function(replaceDropzone){}
 
 // Creates a dropzone each time a task box is created
-function(loopDropzone)
-
+// function(loopDropzone){}
 
 // Changes the theme
-function(changeTheme)
-
+// function(changeTheme){}
